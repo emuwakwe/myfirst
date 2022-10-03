48c4d8f1d9457ca45bd6d240ed6fc09c4b1d3685
Emmanuel Uwakwe <emuwakwe@gmail.com>
2022-10-03 13:14:37 -0400

    Initial Commit

---
+++ app3/app3App.swift
@@ -0,0 +1,17 @@
+//
+//  app3App.swift
+//  app3
+//
+//  Created by Emmanuel Uwakwe on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct app3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
